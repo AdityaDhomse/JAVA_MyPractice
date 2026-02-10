c2722627d2db27f1ed3da24ac046fc9b2c310a72
Test Git <dhomseaditya@gmail.com>
2026-02-10 16:59:10 +0530

    Add Temperature Conversion program

---
+++ 03_TemperatureConversion/src/TemperatureConversion.java
@@ -0,0 +1,13 @@
+public class TemperatureConversion {
+    public static void main(String[] args) {
+        
+        double celsius = 25;
+        double fahrenheit;
+
+        fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("Celsius = " + celsius + " \u00B0C");
+        System.out.println("Fahrenheit = " + fahrenheit + " \u00B0F");
+
+    }
+}
