89b4fb6df1faddfb7ab978182911f78d341bf766
Test Git <dhomseaditya@gmail.com>
2026-02-10 16:56:04 +0530

    Add Student Details

---
+++ 01_BuildProject/src/BuildPractice.java
@@ -0,0 +1,15 @@
+public class BuildPractice {
+    public static void main(String[] args) {
+        
+        String name = "Rahul";
+        int age = 22;
+        double height = 164.5;
+        boolean isStudent = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("isStudent: " + isStudent);
+
+    }
+}
