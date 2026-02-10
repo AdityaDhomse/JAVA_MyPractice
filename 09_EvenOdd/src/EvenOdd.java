64aef660bf4840452ccf9921ee8f57bc4db5aebe
Test Git <dhomseaditya@gmail.com>
2026-02-10 17:10:27 +0530

    Add Even Odd

---
+++ 09_EvenOdd/src/EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+    public static void main(String[] args) throws Exception {
+        
+        int num = 25;
+
+        if(num % 2 == 0) {
+            System.out.println("Number is Even");
+        } else {
+            System.out.println("Number is Odd");
+        }
+
+    }
+}
