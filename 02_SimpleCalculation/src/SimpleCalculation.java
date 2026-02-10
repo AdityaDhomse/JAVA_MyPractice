b4a8917aa392b36b3cce92db1d0182ed0093e955
Test Git <dhomseaditya@gmail.com>
2026-02-10 16:59:09 +0530

    Add Simple Calculation program

---
+++ 02_SimpleCalculation/src/SimpleCalculation.java
@@ -0,0 +1,12 @@
+public class SimpleCalculation {
+    public static void main(String[] args) {
+        
+        int a = 5, b = 10;
+
+        System.out.println("Sum: a + b = " + a + " + " + b + " = " + (a + b));
+        System.out.println("Difference: a - b = " + a + " - " + b + " = " + (a - b));
+        System.out.println("Product: a * b = " + a + " * " + b + " = " + (a * b));
+        System.out.println("Quotient: a / b = " + a + " / " + b + " = " + (double)a / b);
+
+    }
+}
