341b022313e7ea5041a9658c8b065e3492aa366d
Test Git <dhomseaditya@gmail.com>
2026-02-10 16:59:12 +0530

    Add Total Average Marks program

---
+++ 06_TotalAvgMarks/src/TotalAvgMarks.java
@@ -0,0 +1,19 @@
+
+public class TotalAvgMarks {
+
+    public static void main(String[] args) {
+
+        int n1 = 30;
+        int n2 = 93;
+        int n3 = 84;
+        int n4 = 61;
+        int n5 = 23;
+
+        int totalMarks = n1 + n2 + n3 + n4 + n5;
+        double avgMarks = (totalMarks) / 5.0;
+
+        System.out.println("Total Marks = " + totalMarks);
+        System.out.println("Average Marks = " + avgMarks);
+
+    }
+}
