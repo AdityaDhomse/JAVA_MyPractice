b8539f04e560081b49375ba3ff25d925c741d8ad
Test Git <dhomseaditya@gmail.com>
2026-02-11 23:23:44 +0530

    Add Divisible by 5 and 11

---
+++ 12_DivisibleBy5&11/src/DivisibleBy5and11.java
@@ -0,0 +1,15 @@
+
+public class DivisibleBy5and11 {
+
+    public static void main(String[] args) {
+
+        int num = 605;
+
+        if (num % 5 == 0 && num % 11 == 0) {
+            System.out.println("Divisible by 5 and 11");
+        } else {
+            System.out.println("Not divisible by 5 and 11");
+        }
+
+    }
+}
