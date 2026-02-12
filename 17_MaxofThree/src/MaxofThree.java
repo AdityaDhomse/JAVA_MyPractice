8290530aa49057b96134f012f4ff4f8b6e04830a
Test Git <dhomseaditya@gmail.com>
2026-02-12 17:00:58 +0530

    Added details to course1 file

--- 17_MaxofThree/src/MaxofThree.java
+++ 17_MaxofThree/src/MaxofThree.java
@@ -13,8 +13,8 @@ public class MaxofThree {
     public static void main(String[] args) {
 
         int num1 = 5;
-        int num2 = 0;
-        int num3 = 1;
+        int num2 = 10;
+        int num3 = 15;
 
         int maximum = maxOfThree(num1, num2, num3);
 
