d912f2292a97f0a2c9122371cea6d999b80b6bf0
Test Git <dhomseaditya@gmail.com>
2026-02-12 17:32:26 +0530

    Add Max of Three

--- 17_MaxofThree/src/MaxofThree.java
+++ 17_MaxofThree/src/MaxofThree.java
@@ -13,7 +13,7 @@ public class MaxofThree {
     public static void main(String[] args) {
 
         int num1 = 5;
-        int num2 = 10;
+        int num2 = 11;
         int num3 = 15;
 
         int maximum = maxOfThree(num1, num2, num3);
