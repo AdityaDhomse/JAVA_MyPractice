6e4df780da6172b61679471f4a6f6c14d96625ae
Test Git <dhomseaditya@gmail.com>
2026-02-12 16:42:43 +0530

    Add Multiply Two Numbers

---
+++ 15_MultiplyTwoNos/src/MultiplyTwoNos.java
@@ -0,0 +1,16 @@
+
+public class MultiplyTwoNos {
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 10;
+        int result = multiply(num1, num2);
+        System.out.printf("%d * %d = %d\n", num1, num2, result);
+
+    }
+}
