72644e0018f05f2cf7cf7dbb068cc96bd417f5f2
Test Git <dhomseaditya@gmail.com>
2026-02-20 23:43:25 +0530

    Add Print 50-1

---
+++ 20_Print50to1/src/Print_50to1.java
@@ -0,0 +1,9 @@
+public class Print_50to1 {
+    public static void main(String[] args) {
+        int i = 50;
+        while(i >= 1) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
