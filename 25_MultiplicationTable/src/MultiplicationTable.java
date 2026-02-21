de3daa58995a0f0861ed17eb2c396847c3c0c301
Test Git <dhomseaditya@gmail.com>
2026-02-21 08:28:03 +0530

    Add Multiplication Table

---
+++ 25_MultiplicationTable/src/MultiplicationTable.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int i = 1;
+        while (i <= 10) {
+            System.out.printf("%d X %d = %d\n", number, i, number * i);
+            i++;
+        }
+    }
+}
