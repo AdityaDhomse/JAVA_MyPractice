561289f54b784fe0667718edfe379d98caa9b0e3
Test Git <dhomseaditya@gmail.com>
2026-02-21 08:14:01 +0530

    Add Sum of first N numbers

---
+++ 23_Sumof_N_numbers/src/SumNnos.java
@@ -0,0 +1,12 @@
+public class SumNnos {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.printf("Sum: %d", sum);
+    }
+}
