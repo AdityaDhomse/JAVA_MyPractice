b4cb73b4d44d1c4abe5a2349464a089749fefa91
Test Git <dhomseaditya@gmail.com>
2026-02-21 08:56:32 +0530

    Add Reverse Number

--- 25_MultiplicationTable/src/MultiplicationTable.java
+++ 25_MultiplicationTable/src/MultiplicationTable.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
