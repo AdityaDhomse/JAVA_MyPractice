c8aca7c2013d6ee1e194cd621b3fc019670b263e
Test Git <dhomseaditya@gmail.com>
2026-02-21 08:29:11 +0530

    Add Multiplication Table

--- 25_MultiplicationTable/src/MultiplicationTable.java
+++ 25_MultiplicationTable/src/MultiplicationTable.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MultiplicationTable {
 
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
