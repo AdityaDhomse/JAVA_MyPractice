3413b723fa7d9ccdfc05b68b0e470b5651566c2c
Test Git <dhomseaditya@gmail.com>
2026-02-22 15:22:35 +0530

    Add Prime Checker

---
+++ 31_PrimeNoChecker/src/PrimeNoChecker.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class PrimeNoChecker {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if(number <= 1) {
+            System.out.println("Not a Prime Number");
+            return;
+        }
+
+        for(int i = 2; i < number; i++) {
+            if(number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if(isPrime) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not a Prime");
+        }
+    }
+}
